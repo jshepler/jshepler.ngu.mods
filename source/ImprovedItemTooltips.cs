270918aa74b5b00dbbbc08c2b242a37572ba925b
AnyObj <33627710+AnyObj@users.noreply.github.com>
2025-06-04 09:53:30 +0200

    pt3

--- source/ImprovedItemTooltips.cs
+++ source/ImprovedItemTooltips.cs
@@ -363,8 +363,6 @@ namespace jshepler.ngu.mods
 
             return $"\n\n<b>source(s):</b>\n{sources.Join(s => s, "\n")}";
         }
-
-        private static List<int> _boosts = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
         private static float GetAverageRecycledBoostValue(int startIndex, float boostBonus, float recycleChance)
         {
             var totalBoost = 0f;
